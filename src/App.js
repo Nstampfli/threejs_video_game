a3e9a1712c135c176ef73c76aec3e880af435400
nathan <nathan.stampfli@eemi.com>
2024-07-04 01:43:10 +0200

    fix loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState, Suspense } from 'react';
+import React, { useEffect, useRef, Suspense, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Html, useProgress } from '@react-three/drei';
 import CameraController from './components/CameraController';
@@ -7,17 +7,8 @@ import PlayerControls from './components/PlayerControls';
 import ControlsTutorial from './components/ControlsTutorial';
 import Loader from './components/Loader';
 
-function LoaderWrapper({ setLoadingComplete }) {
-  const { progress, loaded, total } = useProgress();
-
-  useEffect(() => {
-    if (loaded === total && total > 0) {
-      setTimeout(() => {
-        setLoadingComplete(true);
-      }, 500); // Optional delay for smoother transition
-    }
-  }, [loaded, total, setLoadingComplete]);
-
+function LoaderWrapper() {
+  const { progress } = useProgress();
   return <Loader progress={progress} />;
 }
 
@@ -41,21 +32,26 @@ function App() {
     };
   }, []);
 
+  function handleProgress(progress) {
+    if (progress === 100) {
+      setTimeout(() => {
+        setLoadingComplete(true);
+      }, 500); // Optional delay for smoother transition
+    }
+  }
+
   return (
-    <>
-      {!loadingComplete && <LoaderWrapper setLoadingComplete={setLoadingComplete} />}
-      <Suspense fallback={null}>
-        <Canvas style={{ display: loadingComplete ? 'block' : 'none' }}>
-          <ambientLight intensity={0.5} />
-          <directionalLight position={[10, 10, 5]} intensity={1} />
-          <pointLight position={[-10, -10, -10]} intensity={0.5} />
-          <Environment />
-          <PlayerControls position={position} rotation={rotation} />
-          <CameraController target={position} />
-        </Canvas>
-      </Suspense>
-      {loadingComplete && <ControlsTutorial />}
-    </>
+    <Suspense fallback={<LoaderWrapper onProgress={handleProgress} />}>
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[10, 10, 5]} intensity={1} />
+        <pointLight position={[-10, -10, -10]} intensity={0.5} />
+        <Environment />
+        <PlayerControls position={position} rotation={rotation} />
+        <CameraController target={position} />
+      </Canvas>
+      <ControlsTutorial />
+    </Suspense>
   );
 }
 
